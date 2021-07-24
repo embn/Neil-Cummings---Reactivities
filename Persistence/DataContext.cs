66945f6ba52c165f134c15711e64f34f4386f859
embn <emilbille@comasys.dk>
2021-07-24 22:16:49 +0200

    End of Section 2

---
+++ Persistence/DataContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence
+{
+    public class DataContext : DbContext
+    {
+        public DataContext([NotNullAttribute] DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Activity> Activities { get; set; }
+
+    }
+}
